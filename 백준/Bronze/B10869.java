0ada6bfa0df94579af94f8db35e71eba3e55dd79
YuSeongChoi <marine8743@naver.com>
2020-09-21 00:36:21 +0900

    사칙연산(기본)

---
+++ 백준/Bronze/B10869.java
@@ -0,0 +1,20 @@
+package Algorithms;
+
+import java.util.Scanner;
+
+public class B10869 {
+
+	public static void main(String[] args) {
+		// 사칙 연산 (기본)
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+	}
+
+}
