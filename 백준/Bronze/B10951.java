901c70567de0418a6ac35deae94af7ac6cbbc046
YuSeongChoi <marine8743@naver.com>
2020-09-22 00:31:38 +0900

    A+B ---4

---
+++ 백준/Bronze/B10951.java
@@ -0,0 +1,17 @@
+package Algorithms;
+
+import java.util.Scanner;
+
+public class B10951 {
+
+	public static void main(String[] args)  {
+		// A+B --- 4
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
